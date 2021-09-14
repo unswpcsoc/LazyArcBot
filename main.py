365614c0e5f0a8ad2d9bc69796b5e24ac6433853
tld8102 <34595951+tld8102@users.noreply.github.com>
2021-09-14 13:11:24 +1000

    Add files via upload
    most up to date

--- main.py
+++ main.py
@@ -3,70 +3,79 @@ import discord
 import datetime
 import time
 import os
-import pandas
 from dotenv import load_dotenv
+from selenium import webdriver
 from discord.ext import commands
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
-from selenium import webdriver
 
-#all secret keys and google drive fileId are store in the '.env' file
+# all secret keys and google drive fileId are store in the '.env' file
 load_dotenv()
 discord_key = os.getenv('Discord_BOT-TOKEN')
 discord_role_id_EXEC = os.getenv('DiscordRoleID_EXEC')
 discord_role_id_MOD = os.getenv('DiscordRoleID_MOD')
-discord_channel_id = os.getenv('DiscordChannelID_execs')
+discord_channel_id = os.getenv('DiscordChannelID_devs')
 team_drive_id_UNSWPCsoc = os.getenv('ShareDriveFileID-UNSW_PCsoc')
 parent_folder_id_21T3 = os.getenv('SubFolderFileID-21T3')
 default_form_file_id = os.getenv('ARCFormFileID-Google_Forms')
-ClubName = os.getenv('ClubName_xPath')
+#############################################################
+ArcFolderDump = os.getenv(r'')
+ClubName = os.getenv('ClubName')
 FirstName = os.getenv('ApplicantFirstName')
 LastName = os.getenv('ApplicantLastName')
 EmailAddress = os.getenv('ApplicantEmail')
 ContactNumber = os.getenv('ApplicantContactNumber')
 
-#sets the varible to a text
+# sets the varible to a text
 default_form_title = "Arc Online Event Attendance List"
-#sets the text string from "env" file to a intager numbers so that EXEC and MOD roles will have access
+# sets the text string from "env" file to a intager numbers so that EXEC and MOD roles will have access
 discord_role_id_EXEC = int(discord_role_id_EXEC)
 discord_role_id_MOD = int(discord_role_id_MOD)
 discord_channel_id = int(discord_channel_id)
-                               
-#Discord API stuffs
+
+#def GoogleAuthentication():
+    # Google drive API authenication stuffs (pyDrive) with creditials stored in 'client_secrets.json' file. also the google authenication is put here so that the keys don't expire... i think. so now it authenticates everytime
+    #gauth = GoogleAuth()
+    #gauth.LocalWebserverAuth()
+    #drive = GoogleDrive(gauth)
+
+# Discord API stuffs
 client = discord.Client()
-@client.event
+
 @commands.guild_only()
+@client.event
 async def on_ready():
-    #tell us when we have a successful login. example: "______#1234"
+    # tell us when we have a successful login. example: "______#1234"
     print('We have logged in to Discord as {0.user}'.format(client))
-    
+
 async def on_message(message):
-    #makes sure the bot doesn't endlessly respond to itself
+    # makes sure the bot doesn't endlessly respond to itself
     if message.author == client.user:
         return
-    
-    #ensures that bot or google drive isn't broken when and title begins with a " " or ([space]).
+
+    # ensures that bot or google drive isn't broken when and title begins with a " " or ([space]).
     if message.content.startswith("~arc  "):
         return
-    
-    #checks if new message starts with '~", if so the bot will run the code below
-    if message.content.startswith("~arc ") and message.channel.id == discord_channel_id and any(i.id == discord_role_id_MOD or discord_role_id_EXEC for i in message.author.roles):
-        #sets variable to whatever message is recieved
-        InputTitle = message.content
-        #sets the variable to original recived message but without the "~" character
-        EventName = InputTitle.replace("~arc ","").upper()
-        #sets the variabel to whomever sent the initial meesage
-        AuthorisedExec = message.author
-        
-        #sets the varible to whatever date is today
-        folder_naming_scheme = datetime.date.today().strftime("%m-%d-%Y")
 
-        #Google drive API authenication stuffs (pyDrive) with creditials stored in 'client_secrets.json' file. also the google authenication is put here so that the keys don't expire... i think. so now it authenticates everytime
+    # checks if new message starts with '~", if so the bot will run the code below
+    if message.content.startswith("~arc ") and message.channel.id == discord_channel_id and any(i.id == discord_role_id_MOD or discord_role_id_EXEC for i in message.author.roles):
+        print("testing recieved")
+        # sets variable to whatever message is recieved
+        InputTitle = message.content
+        # sets the variable to original recived message but without the "~" character
+        EventName = InputTitle.replace("~arc ", "").upper()
+        # sets the variabel to whomever sent the initial meesage
+        AuthorisedExec = message.author
+
+        # Google drive API authenication stuffs (pyDrive) with creditials stored in 'client_secrets.json' file. also the google authenication is put here so that the keys don't expire... i think. so now it authenticates everytime
         gauth = GoogleAuth()
         gauth.LocalWebserverAuth()
         drive = GoogleDrive(gauth)
-        
-        #creates a folder for the new event, appropriate naming scheme of YYY-MM-DD followed my the event name, saves the folder in the right location
+
+        # sets the varible to whatever date is today
+        folder_naming_scheme = datetime.date.today().strftime("%m-%d-%Y")
+
+        # creates a folder for the new event, appropriate naming scheme of YYY-MM-DD followed my the event name, saves the folder in the right location
         NewEventFolder = drive.CreateFile({
             'title': folder_naming_scheme + " " + EventName,
             'mimeType': "application/vnd.google-apps.folder",
@@ -79,10 +88,10 @@ async def on_message(message):
         NewEventFolder.Upload()
 
         print("New Folder:  " + 'title: %s, id: %s' % (NewEventFolder['title'], NewEventFolder['id']))
-        #sets the variable to whatever the newly created folder's ID is
+        # sets the variable to whatever the newly created folder's ID is
         NewEventFolder_id = NewEventFolder['id']
 
-        #copies the template google forms file and saves it to the newly created event folder
+        # copies the template google forms file and saves it to the newly created event folder
         NewEventForm = drive.auth.service.files().copy(
             fileId=default_form_file_id,
             body={
@@ -94,29 +103,33 @@ async def on_message(message):
             }
         ).execute()
 
-        print("New Form:    " + 'title: %s, id: %s' % (NewEventForm['title'],NewEventForm['id']))
-        #sets the varible to whatever the newly copied form's ID is
+        print("New Form:    " + 'title: %s, id: %s' % (NewEventForm['title'], NewEventForm['id']))
+        # sets the varible to whatever the newly copied form's ID is
         NewEventForm_id = NewEventForm['id']
-        #sets the variable to a text that is formated correctly to work as a URL
+        # sets the variable to a text that is formated correctly to work as a URL
         NewEventForm_link = "https://docs.google.com/forms/d/" + NewEventForm_id
         print(AuthorisedExec)
         print(NewEventForm_link)
 
-        #replys back to the orginal message with the form's URL
+        # replys back to the orginal message with the form's URL
         await message.reply(NewEventForm_link)
+        return
+
+    if message.content == ("~grants") and message.channel.id == discord_channel_id and any(i.id == discord_role_id_MOD or discord_role_id_EXEC for i in message.author.roles):
+        Sparc()
+        return
+
 client.run(discord_key)
 
-#this part is all for the Spac Grants function
 def Sparc ():
-   #get google drive api to download the google sheets as .xlsx and .pdf
-    
-    local_arc_files_path = r'C:\Users\ray\Desktop\arc_forms'
-    abc123 = r'\'
-    TestPATH = local_arc_files_path + abc123 + folder_naming_scheme
-    BaseFileSource = pandas.read_excel(f'{TestPATH}.xlsx')
+    #
 
-    SignOn_time = pandas.DataFrame(data, columns=['Product'])
-    
+    BaseFilePath = f'{ArcFolderDump}\{folder_naming_scheme}'
+    BaseFileSource = pandas.read_excel(f'{BaseFilePath}.xlsx')
+
+    ActivityTime = pandas.DataFrame(BaseFileSource, columns = ['Timestamp'])
+    print(ActivityTime)
+
     places = []
     # open file and read the content in a list
     with open("NameList.txt", 'r') as filehandle:
@@ -132,7 +145,7 @@ def Sparc ():
     print(f'{ClubName} is present in list at indexes {matched_indexes}')
     ClubName_xPath = f'//*[@id="field91696630"]/option{matched_indexes}'
     print(ClubName_xPath)
-    
+
     # PATHa = "C:\\tools\selenium\MicrosoftWebDriver.exe"
     PATH = "C:\Program Files (x86)\Selenium\chromedriver.exe"
 
@@ -152,15 +165,19 @@ def Sparc ():
     club = browser.find_element_by_xpath(ClubName_xPath)
     club.click()
 
+    FirstName = "Ray"
     fname = browser.find_element_by_xpath('//*[@id="field91696631-first"]')
     fname.send_keys(FirstName)
 
+    LastName = "He"
     lname = browser.find_element_by_xpath('//*[@id="field91696631-last"]')
     lname.send_keys(LastName)
 
+    EmailAddress = "tld8102@gmail.com"
     email = browser.find_element_by_xpath('//*[@id="field91696632"]')
     email.send_keys(ApplicantEmail)
 
+    ContactNumber = "0435017501"
     mobile = browser.find_element_by_xpath('//*[@id="field91696633"]')
     mobile.send_keys(ContactNumber)
 
@@ -200,12 +217,12 @@ def Sparc ():
 
     EvidenceAttendance = "?file?"
     evidence = browser.find_element_by_xpath('//*[@id="field91698387UploadButton"]')
-    evidence.send_keys(os.getcwd(f'{BaseFileSource}.pdf'))
+    evidence.send_keys(os.getcwd(f'{BaseFilePath}.pdf'))
 
     ActivityPhoto = "?file?"
     photo = browser.find_element_by_xpath('//*[@id="field91698320UploadButton"]')
-    photo.send_keys(os.getcwd(f'{BaseFileSource}.jpg'))
-    
+    photo.send_keys(os.getcwd(f'{BaseFilePath}.jpg'))
+
     IncomeExpenditure = "No"
     money = browser.find_element_by_xpath('//*[@id="field91698326_2"]')
     money.send_click()
@@ -224,4 +241,3 @@ def Sparc ():
         print("Successfully Submitted")
     else:
         print("Script Failed to Submit Form")
-    
